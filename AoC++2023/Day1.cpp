b167c92444351584c8f02fed75bbe600dc5bb3e3
Patryk Kozbur <134033@student.uni.opole.pl>
2023-12-02 16:00:46 +0100

    Add project files.

---
+++ AoC++2023/Day1.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+//part1
+/*int lookForNumbers(std::string line) {
+	int s{0};
+
+	for (int i = 0; i < line.length(); i++) {
+		if (isdigit(line[i])) {
+			char firstNumber = line[i];
+
+			for (int j = (line.length() - 1); j >= 0; j--) {
+
+				if (isdigit(line[j])) {
+					if (j > i) {
+						char secondNumber = line[j];
+						s = (static_cast<int>(firstNumber) - 48) * 10 + (static_cast<int>(secondNumber) - 48);
+						return s;
+					}
+					else {
+						s = (static_cast<int>(firstNumber) - 48) * 10 + (static_cast<int>(firstNumber) - 48);
+						return s;
+					}
+				}
+			}
+		}
+	}
+}
+*/
+
+std::string ReplaceAll(std::string original, std::string replaceThis, std::string replaceWith)
+{
+	std::string::size_type pos = 0;
+	while ((pos = original.find(replaceThis, pos)) != std::string::npos)
+	{
+		original.replace(pos, (replaceThis.length()), replaceWith);
+		pos += replaceWith.length();
+	}
+	return original;
+}
+
+
+int lookForNumbers(std::string line) {
+	line = ReplaceAll(line, "one", "on1e");
+	line = ReplaceAll(line, "two", "tw2o");
+	line = ReplaceAll(line, "three", "thre3e");
+	line = ReplaceAll(line, "four", "fou4r");
+	line = ReplaceAll(line, "five", "fiv5e");
+	line = ReplaceAll(line, "six", "si6x");
+	line = ReplaceAll(line, "seven", "sev7en");
+	line = ReplaceAll(line, "eight", "eig8ht");
+	line = ReplaceAll(line, "nine", "ni9ne");
+	std::cout << line << '\n';
+	int s{ 0 };
+
+	for (int i = 0; i < line.length(); i++) {
+		if (isdigit(line[i])) {
+			char firstNumber = line[i];
+
+			for (int j = (line.length() - 1); j >= 0; j--) {
+
+				if (isdigit(line[j])) {
+					if (j > i) {
+						char secondNumber = line[j];
+						s = (static_cast<int>(firstNumber) - 48) * 10 + (static_cast<int>(secondNumber) - 48);
+						return s;
+					}
+					else {
+						s = (static_cast<int>(firstNumber) - 48) * 10 + (static_cast<int>(firstNumber) - 48);
+						return s;
+					}
+				}
+			}
+		}
+	}
+}
+
+
+int main() {
+	std::string line;
+	std::ifstream file("input.txt");
+	int counter{ 0 };
+	if (file.is_open()) {
+		while (std::getline(file, line)) {
+			std::cout << line << '\n';
+			counter += lookForNumbers(line);
+			std::cout << counter << '\n';
+		}
+		file.close();
+	}
+	return 0;
+}
